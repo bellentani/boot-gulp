fa1f5cd7a7fdb663400b4a29968e5fc2aa90be15
Poe Bellentani <contato@fernandob.com.br>
2016-11-09 10:38:51 -0200

    Adicionado o Handlebars para gerar arquivo estático.

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,9 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
 var sourcemaps = require('gulp-sourcemaps');
+var handlebars = require('gulp-compile-handlebars');
+var rename = require('gulp-rename');
 
-gulp.task('hello', function() {
-  // Stuff here
-  console.log('Hello Zell');
-});
 
 gulp.task('sass', function(){
   return gulp.src('src/scss/**/*.scss')
@@ -19,19 +17,30 @@ gulp.task('sass', function(){
 });
 
 gulp.task('autoprefixer', function () {
-    var postcss      = require('gulp-postcss');
-    var sourcemaps   = require('gulp-sourcemaps');
-    var autoprefixer = require('autoprefixer');
+  var postcss      = require('gulp-postcss');
+  var sourcemaps   = require('gulp-sourcemaps');
+  var autoprefixer = require('autoprefixer');
 
-    return gulp.src('dist/css/**/*.css')
-        .pipe(sourcemaps.init())
-        .pipe(postcss([ autoprefixer({
-          browsers: ['last 5 versions'],
-          cascade: false
-        }) ]))
-        //.pipe(sourcemaps.write('.'))
-        .pipe(gulp.dest('dist/css/'));
+  return gulp.src('dist/css/**/*.css')
+    .pipe(sourcemaps.init())
+    .pipe(postcss([ autoprefixer({
+      browsers: ['last 5 versions'],
+      cascade: false
+    }) ]))
+    //.pipe(sourcemaps.write('.'))
+    .pipe(gulp.dest('dist/css/'));
 });
+
+gulp.task('handlebars', function() {
+  var content = require('./src/templates/data/main.json');;
+
+  return gulp.src('src/templates/**/*.hbs')
+    .pipe(handlebars(content))
+    .pipe(rename({extname: '.html'}))
+    .pipe(gulp.dest('dist'));
+});
+
+//gulp.task('default', ['handlebars']);
 
 gulp.task('watch', function(){
   gulp.watch('src/scss/**/*.scss', ['sass']);
